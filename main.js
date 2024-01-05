e70a645d4946e53ad76ae1e3a57f8c3390bbf65d
Uti-Favour <utifavour15@gmail.com>
2024-01-05 17:38:10 +0100

    High Score Section

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded' , function(){
 let arrowIndicationButton = document.getElementsByClassName('arrow_buttons');
-
+let highScoreContainer = document.getElementsByClassName('high_score_container');
 
 
 
