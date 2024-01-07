8ac57850da07218152d6c845a4ec7745c48a4907
Uti-Favour <utifavour15@gmail.com>
2024-01-07 18:53:21 +0100

    Game Board variable added

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded' , function(){
 let arrowIndicationButton = document.getElementsByClassName('arrow_buttons');
 let highScoreContainer = document.getElementsByClassName('high_score_container');
 
+//Const Variables
+const tetrisGameBoard = document.getElementsByClassName('tetris-board');
 
 
 
